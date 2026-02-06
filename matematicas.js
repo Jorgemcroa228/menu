4d5fe7089015a383aec476a1a0ecacc1b2108b2a
Jorge mario <jorgemcroa228@gmail.com>
2026-02-05 23:54:18 -0500

    Se la suma y la multiplicacion

--- matematicas.js
+++ matematicas.js
@@ -1,1 +1,13 @@
 let menu = prompt("Elige \n 1 : para realizar una suma. \n 2 : para hacer una multiplicacion.");
+
+if(menu === 1) {
+  let number1 = prompt("ingresa el primer numero :");
+  let number2 = prompt("ingresa el segundo numero :");
+  let result = prompt("La suma de " + number1 + " y " + number2 + " es de : " + (number1 + number2));
+} else if (menu === 2) {
+  let number1 = prompt("ingresa el primer numero :");
+  let number2 = prompt("ingresa el segundo numero :");
+  let result = prompt("La multiplicacion de " + number1 + " y " + number2 + " es de : " + (number1 * number2));
+} else {
+  alert("solo puede seleccionar 1 y 2.")
+}
