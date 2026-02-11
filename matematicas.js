04ae9c26d20800c96c0e0c25932293c50cdae80f
Jorge mario <jorgemcroa228@gmail.com>
2026-02-10 23:24:30 -0500

    se terminaron de agregar todos los (parseFloat) y se hizo prueba del codigo

--- matematicas.js
+++ matematicas.js
@@ -6,7 +6,7 @@ if(menu === 1) {
   let result = prompt("La suma de " + number1 + " y " + number2 + " es de : " + (number1 + number2));
 } else if (menu === 2) {
   let number1 = parseFloat(prompt("ingresa el primer numero :"));
-  let number2 = prompt("ingresa el segundo numero :");
+  let number2 = parseFloat(prompt("ingresa el segundo numero :"));
   let result = prompt("La multiplicacion de " + number1 + " y " + number2 + " es de : " + (number1 * number2));
 } else {
   alert("solo puede seleccionar 1 y 2.")
