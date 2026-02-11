482bd64e4287523f4b0c07a26de1c2f089e70611
Jorge mario <jorgemcroa228@gmail.com>
2026-02-10 23:17:58 -0500

    se agregan (parseFloat) para convertir las cadenas de texto a numeros enteros

--- matematicas.js
+++ matematicas.js
@@ -1,11 +1,11 @@
-let menu = prompt("Elige \n 1 : para realizar una suma. \n 2 : para hacer una multiplicacion.");
+let menu = parseFloat(prompt("Elige \n 1 : para realizar una suma. \n 2 : para hacer una multiplicacion."));
 
 if(menu === 1) {
-  let number1 = prompt("ingresa el primer numero :");
-  let number2 = prompt("ingresa el segundo numero :");
+  let number1 = parseFloat(prompt("ingresa el primer numero :"));
+  let number2 = parseFloat(prompt("ingresa el segundo numero :"));
   let result = prompt("La suma de " + number1 + " y " + number2 + " es de : " + (number1 + number2));
 } else if (menu === 2) {
-  let number1 = prompt("ingresa el primer numero :");
+  let number1 = parseFloat(prompt("ingresa el primer numero :"));
   let number2 = prompt("ingresa el segundo numero :");
   let result = prompt("La multiplicacion de " + number1 + " y " + number2 + " es de : " + (number1 * number2));
 } else {
